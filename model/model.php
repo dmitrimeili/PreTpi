4c87349eeea31bc098a450f148da049541eba569
dmitrimeili <dmitri.meili@cpnv.ch>
2022-04-14 11:43:24 +0200

    Update model.php

--- model/model.php
+++ model/model.php
@@ -114,7 +114,7 @@ function getReviewByType($id)
         inner join videogames ON reviews.VideoGames_id = videogames.id
         inner join platforms ON videogames.Platforms_id = platforms.id
         where platforms.id = :id 
-        where approuved = 0';
+        and approuved = 1';
         $statement = $dbh->prepare($query);//prepare query
         $statement->execute(['id' => $id]);//execute query
         $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC);//prepare result for client
